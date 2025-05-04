dbccbbb4cf3a962759c47b98cb4338f04bb0d726
Ali Hussein <ali@alMBPalkhasbAli.home>
2025-05-04 04:19:54 +0300

    my website

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -3,16 +3,18 @@
     <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
       <div class="text-4xl font-light font-serif text-white">Ali Hussein</div>
       <div class="flex justify-center flex-grow">
-        <ul class="flex space-x-4 md:space-x-6 text-sm font-medium">
-          <li><a href="https://www.linkedin.com/in/alihusseintechandfinance/" target="_blank" class="hover:text-indigo-400 transition hidden md:block">LinkedIn</a></li>
-          <li><a href="https://github.com/AliHusseinAs" target="_blank" class="hover:text-indigo-400 transition hidden md:block">GitHub</a></li>
-          <li>
-            <a href="/cv.pdf" target="_blank" class="hover:text-indigo-400 transition hidden md:block">CV</a>
+        <ul class="flex space-x-2 md:space-x-4 text-sm font-medium items-center">
+          <li class="hidden md:block"><a href="#skills" class="hover:text-indigo-400 transition">Skills</a></li>
+          <li class="hidden md:block"><a href="#projects" class="hover:text-indigo-400 transition">Projects</a></li>
+          <li class="hidden md:block"><a href="https://www.linkedin.com/in/alihusseintechandfinance/" target="_blank" class="hover:text-indigo-400 transition">LinkedIn</a></li>
+          <li class="hidden md:block"><a href="https://github.com/AliHusseinAs" target="_blank" class="hover:text-indigo-400 transition">GitHub</a></li>
+          <li class="hidden md:block">
+            <a href="/cv.pdf" target="_blank" class="hover:text-indigo-400 transition">CV</a>
           </li>
           <li>
             <a
               href="mailto:alihussein10x@gmail.com"
-              class="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-3 rounded-full shadow-md transition-colors duration-300 text-xs md:text-sm"
+              class="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-3 rounded-full shadow-md transition-colors duration-300 text-xs"
             >
               Contact Me
             </a>
